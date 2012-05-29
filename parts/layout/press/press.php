164ab96d82e57a024164907e485fcfd245fdc597
Kevin Doyle <kdoyle2@gmail.com>
2012-05-29 09:04:37 -0400

    initial

---
+++ parts/layout/press/press.php
@@ -0,0 +1,22 @@
+<section id="press">
+	<h1>Press</h1>
+	<article>
+		<h2>Lorem ipsum dolor sit amet <span class="time">00/00/00</span></h2>
+		<div class="extra">
+			<p><a href="#" class="more">Download PDF</a></p>
+		</div>
+	</article>
+	<article>
+		<h2>Lorem ipsum dolor sit amet <span class="time">00/00/00</span></h2>
+		<div class="extra">
+			<p><a href="#" class="more">Visit Website</a></p>
+		</div>
+	</article>
+	<article>
+		<h2>Lorem ipsum dolor sit amet <span class="time">00/00/00</span></h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sed tempus tellus. Praesent eleifend vehicula imperdiet. Etiam aliquet molestie dui, eget sagittis est consectetur in. Duis eleifend facilisis viverra. Nulla egestas leo ut magna posuere posuere. Sed tempus accumsan tortor eu accumsan. Donec euismod cursus massa, et rutrum nisl ultrices sodales. Nulla pellentesque interdum placerat. Phasellus et metus facilisis neque volutpat sagittis. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas libero lorem, blandit sit amet sollicitudin a, lobortis non mauris. Mauris orci mauris, viverra in facilisis non, pretium a libero. Mauris pulvinar feugiat enim, at ultricies nulla volutpat eu. Sed sagittis fringilla nibh ut vehicula. </p>
+		<div class="extra">
+			<a href="#" class="more">Read More &raquo;</a>
+		</div>
+	</article>
+</section>
