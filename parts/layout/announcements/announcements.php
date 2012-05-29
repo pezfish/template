cadbcf077d7b81e9588a9858a1b298879ecbb647
Kevin Doyle <kdoyle2@gmail.com>
2012-05-29 09:04:03 -0400

    layout

--- parts/layout/announcements/announcements.php
+++ parts/layout/announcements/announcements.php
@@ -1,4 +1,5 @@
 <section id="announcements">
+	<h1>Announcements</h1>
 	<?php
 		if (!empty($announcements)) {
 			foreach ($announcements as $curannouncement) {
@@ -8,8 +9,8 @@
 		<?php if (!empty($curthumb)) { ?><img src="http://placehold.it/150x225/" alt="" class="right" /><?php } ?>
 		<h2><?= htmlout($curtitle) ?> <span class="time"><?= date("m/d/y", $curdatestamp) ?></span></h2>
 		<p><?= htmlout($curdescription, true) ?></p>
-		<div class="announcement_extra">
-			<a href="#<?= $curid ?>" class="more">READ MORE &raquo;</a>
+		<div class="extra">
+			<a href="#<?= $curid ?>" class="more">Read More &raquo;</a>
 			<div class="fb-like" data-href="#<?= $curid ?>" data-send="false" data-layout="button_count" data-width="55" data-show-faces="false" data-action="like"></div>
 		</div>
 	</article>
