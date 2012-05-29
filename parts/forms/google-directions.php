61efde334409ad5d35a6c25a58df9f9c232c9480
Kevin Doyle <kdoyle2@gmail.com>
2012-05-29 09:03:45 -0400

    add cellpadding as standard

--- parts/forms/google-directions.php
+++ parts/forms/google-directions.php
@@ -2,7 +2,7 @@
 	<input type="hidden" name="daddr" value="_address_" />
 	<input type="hidden" name="hl" value="en" />
 	<label for="saddr">Please enter your address below for driving directions</label>
-	<table>
+	<table cellpadding="5">
 		<tr>
 			<td width="100%"><input type="text" name="saddr" id="saddr" value="" /></td>
 			<td><input class="button" type="submit" name="submit" value="Get Directions" style="margin:0 0 0 20px;" /></td>
