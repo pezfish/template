30f889f7c7e50e4e3995f99976d388508df2e876
Kevin Doyle <kdoyle2@gmail.com>
2012-06-04 15:37:55 -0400

    initial

---
+++ parts/layout/gallery/gallery-one/gallery-one.php
@@ -0,0 +1,7 @@
+<section id="gallery">	
+	<ul class="clearfix">
+		<? for($i = 0; $i < 15; $i++){ ?>
+		<li><a href="#" rel="gallery"><img src="http://placehold.it/100" alt="" /></a></li>
+		<? } ?>
+	</ul>
+</section>
