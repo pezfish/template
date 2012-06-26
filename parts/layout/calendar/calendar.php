41271cf60082431ae4bc54350e2be5bccc152f95
Kevin Doyle <kdoyle2@gmail.com>
2012-06-26 14:18:47 -0400

    initial commit

---
+++ parts/layout/calendar/calendar.php
@@ -0,0 +1,57 @@
+<table cellspacing="0" cellpadding="0">
+	<caption><a href="#" title="previous month">&laquo;</a> March <a href="#" title="next month">&raquo;</a></caption>
+	<tr>
+		<th scope="col" abbr="Sunday" title="Sunday">S</th>
+		<th scope="col" abbr="Monday" title="Monday">M</th>
+		<th scope="col" abbr="Tuesday" title="Tuesday">T</th>
+		<th scope="col" abbr="Wednesday" title="Wednesday">W</th>
+		<th scope="col" abbr="Thursday" title="Thursday">T</th>
+		<th scope="col" abbr="Friday" title="Friday">F</th>
+		<th scope="col" abbr="Saturday" title="Saturday">S</th>
+	</tr>
+	<tr>
+		<td class="empty">&nbsp;</td>
+		<td class="empty">&nbsp;</td>
+		<td>1</td>
+		<td>2</td>
+		<td><a href="#">3</a></td>
+		<td><a href="#">4</a></td>
+		<td><a href="#">5</a></td>
+	</tr>
+	<tr>
+		<td><a href="#">6</a></td>
+		<td>7</td>
+		<td>8</td>
+		<td><a href="#">9</a></td>
+		<td><a href="#">10</a></td>
+		<td>11</td>
+		<td>12</td>
+	</tr>
+	<tr>
+		<td><a href="#">13</a></td>
+		<td>14</td>
+		<td>15</td>
+		<td>16</td>
+		<td><a href="#">17</a></td>
+		<td><a href="#">18</a></td>
+		<td>19</td>
+	</tr>
+	<tr>
+		<td>20</td>
+		<td>21</td>
+		<td>22</td>
+		<td><a href="#">23</a></td>
+		<td>24</td>
+		<td><a href="#">25</a></td>
+		<td>26</td>
+	</tr>
+	<tr>
+		<td>27</td>
+		<td><a href="#">28</a></td>
+		<td>29</td>
+		<td><a href="#">30</a></td>
+		<td>31</td>
+		<td class="empty">&nbsp;</td>
+		<td class="empty">&nbsp;</td>
+	</tr>
+</table>
