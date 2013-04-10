8426b785103c16aae7059b32633b74fc0624836f
Kevin Doyle <kdoyle2@gmail.com>
2013-04-10 16:39:31 -0400

    cleanup + jquery 1.9.1

--- base/index.php
+++ base/index.php
@@ -13,35 +13,34 @@
 	include("includes/header.php");
 ?>
 <!doctype html>
-<!--[if lt IE 7 ]> <html lang="en" class="no-js ie6"> <![endif]-->
-<!--[if IE 7 ]> <html lang="en" class="no-js ie7"> <![endif]-->
-<!--[if IE 8 ]> <html lang="en" class="no-js ie8"> <![endif]-->
-<!--[if IE 9 ]> <html lang="en" class="no-js ie9"> <![endif]-->
-<!--[if (gt IE 9)|!(IE)]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]> <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]> <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
 <head>
 <meta charset="utf-8" />
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-<title><?= isset($page_meta["title"]) ? $page_meta["title"] : "SITE NAME" ?></title>
-<meta name="keywords" content="<?= isset($page_meta["keywords"]) ? $page_meta["keywords"] : "" ?>">
-<meta name="description" content="<?= isset($page_meta["description"]) ? $page_meta["description"] : "" ?>">
+<title><?= !empty($page_meta["title"]) ? $page_meta["title"] : "" ?></title>
+<meta name="keywords" content="<?= !empty($page_meta["keywords"]) ? $page_meta["keywords"] : "" ?>">
+<meta name="description" content="<?= !empty($page_meta["description"]) ? $page_meta["description"] : "" ?>">
 
 <meta name="author" content="http://www.aycmedia.com" />
 <meta name="viewport" content="width=1000" />
 
 <? /*
-<meta property="og:title" content="" />
-<meta property="og:description" content="" />
-<meta property="og:image" content="" />
+	<meta property="og:title" content="" />
+	<meta property="og:description" content="" />
+	<meta property="og:image" content="" />
 */ ?>
 
-<? /* <link rel="shortcut icon" href="/favicon.ico" /> */ ?>
+<link rel="shortcut icon" href="/favicon.ico" />
 <link href="style.css" rel="stylesheet" type="text/css" />
-<!-- ::::::::::::::::::::::::::: MODERNIZR ::::::::::::::::::::::::::: -->
+<!-- MODERNIZR -->
 <? // html5shiv included by default and is required. Create custom build if additional features are needed. http://modernizr.com/ ?>
 <script type="text/javascript" src="js/modernizr.min.js"></script>
 
 <? /*
-<!-- ::::::::::::::::::::::::::: SWFOBJECT ::::::::::::::::::::::::::: -->
+<!-- SWFOBJECT -->
 <script type="text/javascript" src="js/swfobject.js"></script>
 */ ?>
 </head>
@@ -50,27 +49,27 @@
 	<p>Hi! I&rsquo;m a website.</p>
 
 <? /*
-<!-- ::::::::::::::::::::::::::: GOOGLE MAPS ::::::::::::::::::::::::::: -->
+<!-- GOOGLE MAPS -->
 <? if($page_section == 'contact') { ?>
 	<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
 <? } ?>
 */ ?>
 <? /*
-<!-- ::::::::::::::::::::::::::: JQUERY ::::::::::::::::::::::::::: -->
-<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+<!-- JQUERY -->
+<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 <script>window.jQuery || document.write('<script src="js/jquery.min.js"><\/script>')</script>
 */ ?>
 <? /*
-<!-- ::::::::::::::::::::::::::: SITE SCRIPTS ::::::::::::::::::::::::::: -->
+<!-- SITE SCRIPTS -->
 <script type="text/javascript" src="js/scripts.js"></script>
 */ ?>
 <? /*
-<!-- ::::::::::::::::::::::::::: GOOGLE ANALYTICS ::::::::::::::::::::::::::: -->
+<!-- GOOGLE ANALYTICS -->
 <script>
-    var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview'],['_trackPageLoadTime']];
-    (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;
-    g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
-    s.parentNode.insertBefore(g,s)}(document,'script'));
+	var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+	(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+	g.src='//www.google-analytics.com/ga.js';
+	s.parentNode.insertBefore(g,s)}(document,'script'));
 </script>
 */ ?>
 </body>
