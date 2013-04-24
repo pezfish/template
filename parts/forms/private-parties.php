8f2f5066c985363f1a1085a76b0f89878463ce88
Jonathan Hopkins <jonathan@fogeytron.org>
2013-04-24 11:24:27 -0400

    Update parties form to have programming code for start and end time

--- parts/forms/private-parties.php
+++ parts/forms/private-parties.php
@@ -41,58 +41,26 @@
             <td>
                 <select name="stime" id="stime" style="width:150px;">
                     <option value="">Start Time</option>                      			
-                    <option value="12 AM">12 AM</option>
-                    <option value="1 AM">1 AM</option>
-                    <option value="2 AM">2 AM</option>
-                    <option value="3 AM">3 AM</option>
-                    <option value="4 AM">4 AM</option>
-                    <option value="5 AM">5 AM</option>
-                    <option value="6 AM">6 AM</option>
-                    <option value="7 AM">7 AM</option>
-                    <option value="8 AM">8 AM</option>
-                    <option value="9 AM">9 AM</option>
-                    <option value="10 AM">10 AM</option>
-                    <option value="11 AM">11 AM</option>
-                    <option value="12 PM">12 PM</option>
-                    <option value="1 PM">1 PM</option>
-                    <option value="2 PM">2 PM</option>
-                    <option value="3 PM">3 PM</option>
-                    <option value="4 PM">4 PM</option>
-                    <option value="5 PM">5 PM</option>
-                    <option value="6 PM">6 PM</option>
-                    <option value="7 PM">7 PM</option>
-                    <option value="8 PM">8 PM</option>
-                    <option value="9 PM">9 PM</option>
-                    <option value="10 PM">10 PM</option>
-                    <option value="11 PM">11 PM</option>
+                    <?php
+						for ($i = 0; $i < 24; $i++) {
+							$time = date("g A", mktime($i, 0, 0, 1, 1, 2000));
+					?>
+						<option value="<?= $time ?>"<?= $time == $stime ? " selected=\"selected\"" : "" ?>><?= $time ?></option>
+					<?php
+						}
+					?>
                 </select> 
                 to
                 <select name="etime" id="etime" style="width:150px;">
                     <option value="">End Time</option>                      			
-                    <option value="12 AM">12 AM</option>
-                    <option value="1 AM">1 AM</option>
-                    <option value="2 AM">2 AM</option>
-                    <option value="3 AM">3 AM</option>
-                    <option value="4 AM">4 AM</option>
-                    <option value="5 AM">5 AM</option>
-                    <option value="6 AM">6 AM</option>
-                    <option value="7 AM">7 AM</option>
-                    <option value="8 AM">8 AM</option>
-                    <option value="9 AM">9 AM</option>
-                    <option value="10 AM">10 AM</option>
-                    <option value="11 AM">11 AM</option>
-                    <option value="12 PM">12 PM</option>
-                    <option value="1 PM">1 PM</option>
-                    <option value="2 PM">2 PM</option>
-                    <option value="3 PM">3 PM</option>
-                    <option value="4 PM">4 PM</option>
-                    <option value="5 PM">5 PM</option>
-                    <option value="6 PM">6 PM</option>
-                    <option value="7 PM">7 PM</option>
-                    <option value="8 PM">8 PM</option>
-                    <option value="9 PM">9 PM</option>
-                    <option value="10 PM">10 PM</option>
-                    <option value="11 PM">11 PM</option>
+                    <?php
+						for ($i = 0; $i < 24; $i++) {
+							$time = date("g A", mktime( $i, 0, 0, 1, 1, 2000));
+					?>
+						<option value="<?= $time ?>"<?= $time == $etime ? " selected=\"selected\"" : "" ?>><?= $time ?></option>
+					<?php
+						}
+					?>
                 </select>
             </td>
 		</tr>
