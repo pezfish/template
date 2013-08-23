ab1d9b0ee32b14d65e1274f7e8fc21534487a5e8
Kevin Doyle <kdoyle2@gmail.com>
2013-08-23 09:10:18 -0400

    update to jquery 1.10.2

--- base/index.php
+++ base/index.php
@@ -67,7 +67,7 @@
 */ ?>
 <? /*
 <!-- JQUERY -->
-<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 <script>window.jQuery || document.write('<script src="js/jquery.min.js"><\/script>')</script>
 */ ?>
 <? /*
